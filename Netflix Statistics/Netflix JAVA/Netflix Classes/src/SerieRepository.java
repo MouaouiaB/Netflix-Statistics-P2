d941140f8886d22991c887084ccea7261a97bd5b
abouhazz <ayman_elb112@hotmail.com>
2019-03-21 13:22:02 +0100

    SerieRepo Edited

--- Netflix Statistics/Netflix JAVA/Netflix Classes/src/SerieRepository.java
+++ Netflix Statistics/Netflix JAVA/Netflix Classes/src/SerieRepository.java
@@ -4,17 +4,23 @@ import java.util.*;
 import Connection.SqlConnection;
 
 public class SerieRepository {
-    private SqlConnection sqlConnection;
+    private String sqlConnection;
 
-    public SerieRepository(SqlConnection sqlConnection) {
+    public SerieRepository(String sqlConnection) {
         this.sqlConnection = sqlConnection;
     }
 
     public ArrayList<Serie> readAll() {
+        Connection connection = null;
+        ResultSet resultSet = null;
+        Statement statement = null;
+
         ArrayList<Serie> lijst = new ArrayList<>();
         try {
-            ResultSet rs = sqlConnection.executeSql("SELECT * FROM Serie");
-            while(rs.next()) {
+            connection = DriverManager.getConnection(sqlConnection);
+            statement = connection.createStatement();
+            resultSet = statement.executeQuery("SELECT * FROM Serie");
+            while(resultSet.next()) {
                 //lijst.add(new Serie(rs.getString("Title"), rs.getString("Genre"), rs.getString("Taal"), rs.getString("Seizoen"), rs.getString("Leeftijds Indicatie"), rs.getString("Lijktop"), rs.getString("ProgrammaID")));
             }
         }
@@ -26,13 +32,19 @@ public class SerieRepository {
 
     public Serie read(int ProgrammaID) {
          Serie serie = null;
+        Connection connection = null;
+        ResultSet resultSet = null;
+        Statement statement = null;
+
         try
         {
             //let op: het samenvoegen van strings binnen SQL commando's is ONVEILIG. Pas dit niet toe in een productieomgeving.
             //later in het curriculum wordt behandeld op welke wijze je je hiertegen kunt beschermen.
+            connection = DriverManager.getConnection(sqlConnection);
+            statement = connection.createStatement();
             String sqlQuery = "SELECT * FROM Serie WHERE ProgrammaID= " + ProgrammaID;
-            ResultSet rs = sqlConnection.executeSql(sqlQuery);
-            rs.next();
+            resultSet = statement.executeQuery(sqlQuery);
+            resultSet.next();
             //lijst.add(new Film(rs.getInt("FilmID"),rs.getString("Title"), rs.getString("Tijdsduur"), rs.getString("Genre"), rs.getString("Taal"), rs.getString("Leeftijds Indicatie"), rs.getString("ProgrammaID")));
         }
         catch(Exception e) {
@@ -42,6 +54,10 @@ public class SerieRepository {
     }
 
     public void create(Serie serie) {
+        Connection connection = null;
+        ResultSet resultSet = null;
+        Statement statement = null;
+
         try
         {
             //let op: het samenvoegen van strings binnen SQL commando's is ONVEILIG. Pas dit niet toe in een productieomgeving.
@@ -59,12 +75,19 @@ public class SerieRepository {
     }
 
     public void delete(int programmaID) {
+        Connection connection = null;
+        ResultSet resultSet = null;
+        Statement statement = null;
+
         try
         {
             //let op: het samenvoegen van strings binnen SQL commando's is ONVEILIG. Pas dit niet toe in een productieomgeving.
             //later in het curriculum wordt behandeld op welke wijze je je hiertegen kunt beschermen.
+            connection = DriverManager.getConnection(sqlConnection);
+            statement = connection.createStatement();
             String sqlQuery = "DELETE Serie WHERE ProgrammaID= " + programmaID;
-            sqlConnection.executeSqlNoResult(sqlQuery);
+            resultSet = statement.executeQuery(sqlQuery);
+            resultSet.next();
         }
         catch(Exception e) {
             System.out.println(e);
